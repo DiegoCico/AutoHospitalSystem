2cf42fd5f4e326850769099d68a7f1144e3b32ee
Diego Cicotoste <cicotosted@gmail.com>
2024-03-05 12:42:27 +0000

    major fix

--- src/main/java/HospitalHandler.java
+++ src/main/java/HospitalHandler.java
@@ -4,35 +4,63 @@ import java.util.Stack;
 public class HospitalHandler {
     private ArrayList<Patient> waitlist;
     private ArrayList<Room> rooms;
-    private int numberOfRooms;
     private ArrayList<String> doctors;
 
     public HospitalHandler(int numberOfRooms){
-        this.numberOfRooms = numberOfRooms;
         rooms = new ArrayList<>();
+        for(int i = 0; i < numberOfRooms; i++){
+            rooms.add(new Room());
+        }
         waitlist = new ArrayList<>();
         doctors = new ArrayList<>();
     }
 
-    public HospitalHandler(int numberOfRooms,ArrayList<Patient> waitlist, ArrayList<String> doctors){
-        this.numberOfRooms = numberOfRooms;
-        rooms = new ArrayList<>();
+    public HospitalHandler(int numberOfRooms, ArrayList<Patient> waitlist, ArrayList<String> doctors){
+        this(numberOfRooms);
         this.waitlist = waitlist;
         this.doctors = doctors;
     }
 
     public void addPatient(Patient patient){
         waitlist.add(patient);
-        sortPatientByPriority();
     }
 
     public void sortPatientByPriority(){
-        for(int i = 0; i < waitlist.size() -1; i++){
-                if(waitlist.get(i).getPriority() < waitlist.get(i+1).getPriority()){
+        for(int i = 0; i < waitlist.size()-1; i++){
+            for(int j = i+1; j<waitlist.size();j++){
+                if(waitlist.get(i).getPriority() < waitlist.get(j).getPriority()){
                     Patient temp = waitlist.get(i);
-                    waitlist.set(i, waitlist.get(i+1));
-                    waitlist.set(i+1, temp);
+                    waitlist.set(i, waitlist.get(j));
+                    waitlist.set(j, temp);
                 }
+            }
+        }
+    }
+
+    public void addPatientToRoom() {
+        sortPatientByPriority();
+        if (waitlist.isEmpty()) {
+            System.out.println("No patient in the wait list");
+            return;
+        }
+
+        for (Room room : rooms) {
+            if (room.getPatient() == null && !waitlist.isEmpty()) {
+                room.setPatient(waitlist.remove(0));
+            }
+        }
+    }
+
+
+    public void addDocToRoom(){
+        if (doctors.isEmpty() || rooms.isEmpty()) {
+            return;
+        }
+
+        int doctorIndex = 0;
+        for (Room room : rooms) {
+            room.setDoctor(doctors.get(doctorIndex));
+            doctorIndex = (doctorIndex + 1) % doctors.size();
         }
     }
 
